27ac5c36be208c03af419674d7242c70dd02932e
Josh <64.josh@gmail.com>
2019-06-17 14:45:02 -0600

    Added CRUD

---
+++ Data/VehicleMvcDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VehicleMvcDemo.Models
+{
+    public class VehicleMvcDemoContext : DbContext
+    {
+        public VehicleMvcDemoContext (DbContextOptions<VehicleMvcDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VehicleMvcDemo.Models.Vehicle> Vehicle { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using VehicleMvcDemo.Models;
 
 namespace VehicleMvcDemo
 {
@@ -33,6 +35,9 @@ namespace VehicleMvcDemo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<VehicleMvcDemoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("VehicleMvcDemoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
