9b90d05ad5e7aad5ccc02a6595598a9dcbb5c2aa
Josh <64.josh@gmail.com>
2019-06-17 14:35:10 -0600

    Added vehicle class model

---
+++ Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleMvcDemo.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleId { get; set; }
+        public string VehicleMake { get; set; }
+        public string VehicleModel { get; set; }
+        public string VehicleYear { get; set; }
+        public string VehicleVin { get; set; }
+        public string VehicleNote { get; set; }
+    }
+}
