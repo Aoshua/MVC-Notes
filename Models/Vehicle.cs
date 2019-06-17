cc4cbaf4cc862e7e1480528b842d6a7b67c8f769
Josh <64.josh@gmail.com>
2019-06-17 15:02:05 -0600

    Updated vehicle class model

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -10,10 +10,25 @@ namespace VehicleMvcDemo.Models
     {
         [Key]
         public int VehicleId { get; set; }
+
+        [Required]
+        [Display(Name = "Make")]
         public string VehicleMake { get; set; }
+
+        [Required]
+        [Display(Name = "Model")]
         public string VehicleModel { get; set; }
+
+        [Required]
+        [Display(Name = "Year")]
         public string VehicleYear { get; set; }
+
+        [Required]
+        [Display(Name = "Vehicle Identification Number (VIN)")]
+        [RegularExpression(@"[a-zA-Z0-9]{9}[a-zA-Z0-9-]{2}[0-9]{6}")]
         public string VehicleVin { get; set; }
+
+        [Display(Name = "Notes")]
         public string VehicleNote { get; set; }
     }
 }
